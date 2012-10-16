b3a9be22d0be71528ba767b4941770c81cdb036a
Jesus Urcera <jurcera@gmail.com>
2012-10-17 01:49:47 +0200

    Reordenacion del programa principal

--- SusoCrawler12.py
+++ SusoCrawler12.py
@@ -2,10 +2,17 @@ import urllib2
 from BeautifulSoup import BeautifulSoup as Soup
 import argparse
 
+parser = argparse.ArgumentParser(description ="Capturador de urls en Internet")
+parser.add_argument('url', nargs =1, help='URL de busqueda')
+parser.add_argument('-n', '--number-of-levels', type=int, default=1, help='Profundidad de busqueda')
+args = parser.parse_args()
+target_url = args.url.pop()
+deep = args.number_of_levels
+
 user_agent = " Mozilla /5.0 ( X11; U; Linux x86_64 ; en-US) AppleWebKit /534.7 (KHTML , like Gecko ) Chrome/7.0.517.41 Safari /534.7 "
 _opener = urllib2.build_opener()
 _opener.addheaders = [( 'User-agent', user_agent )]
-raw_code = _opener.open("http://www.google.es/").read()
+raw_code = _opener.open(target_url).read()
 
 soup_code = Soup(raw_code)
 links = [link['href'] for link
@@ -14,11 +21,5 @@ links = [link['href'] for link
 
 print links
 
-parser = argparse.ArgumentParser(description ="Capturador de urls en Internet")
-parser.add_argument('url', nargs =1, help='URL destino')
-parser.add_argument('-n', '--number-of-levels', type=int, default=1, help='Profundidad de busqueda')
-args = parser.parse_args()
-target_url = args.url.pop()
-deep = args.number_of_levels
-print target_url
-print deep
+
+
