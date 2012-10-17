1ab66b5fbffe3e9e9dbc9281344dc53e520fe33d
Jesus Urcera <jurcera@gmail.com>
2012-10-17 21:01:21 +0200

    Añadir licencia al fichero SusoCrawler12.py. Actualizado fichero README.

--- SusoCrawler12.py
+++ SusoCrawler12.py
@@ -1,6 +1,23 @@
 #! /usr/bin/python
 # coding=UTF-8
+'''
+Copyright (C) 2012  Jesus Urcera Lopez. All rights reserved.
 
+This file is part of SusoCrawler12.
+
+SusoCrawler12 is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
 
 import urllib2
 from BeautifulSoup import BeautifulSoup as Soup
